b21105805dd100d6e52ec86331a4db0c53b866ef
Shakti Shrestha <75934459+ShaktiShresth@users.noreply.github.com>
2026-04-27 22:30:45 +0545

    small change in OAuth button

--- frontend/src/components/SignInOAuthButtons.tsx
+++ frontend/src/components/SignInOAuthButtons.tsx
@@ -17,8 +17,8 @@ const SignInOAuthButtons = () => {
     };
 
     return (
-        <Button onClick={signInWithGoogle} variant={"secondary"} className='w-full text-white border-zinc-200 h-11'>
-            <img src='/google.png' alt='Google' className='size-5' />
+        <Button onClick={signInWithGoogle} variant={"secondary"} className='w-full text-white border-zinc-200 h-11  bg-zinc-800 hover:cursor-pointer hover:bg-zinc-900 group'>
+            <img src='/google.png' alt='Google' className='size-5 transition-all ease-in-out group-hover:scale-110' />
             Continue with Google
         </Button>
     );
